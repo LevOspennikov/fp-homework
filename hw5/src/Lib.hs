995bbf063226882d54effce67245995b9c5288cf
Lev Ospennikov <ospennikovlev@gmail.com>
2017-10-30 15:46:48 +0300

    delete other

--- hw5/src/Lib.hs
+++
@@ -1,9 +0,0 @@
-module Lib
-    ( maybeConcat
-    ) where
-
-
-maybeConcat :: (Monoid t) => [Maybe t] -> t
-maybeConcat []            = mempty
-maybeConcat ((Nothing):x) = maybeConcat x
-maybeConcat ((Just t):x)  = mappend t (maybeConcat x)
